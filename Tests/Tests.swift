e8fa831375632c86aa75ed33fd2503c7654bd5f7
tokorom <tokorom@gmail.com>
2016-09-10 17:13:53 +0900

    Add Tests

---
+++ Tests/Tests.swift
@@ -0,0 +1,21 @@
+//
+//  Tests.swift
+//
+//  Created by ToKoRo on 2016-09-10.
+//
+
+import XCTest
+
+class Tests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testFunctions() {
+    }
+}
